3852a06102ec5be3a7e10d6e0e89a433c7914f2a
= <=>
2022-06-02 15:28:40 -0800

    forgot main

---
+++ main.lua
@@ -0,0 +1,17 @@
+
+local Controller = require "controller"
+local Model = require "model"
+local View = require "view"
+local HJKL = require "HJKL"
+
+local controller = Controller()
+local model = Model(controller)
+local view = View(controller)
+local hjkl = HJKL(controller)
+
+controller.set_model(model)
+controller.set_view(view)
+
+controller.add_event_listener(hjkl)
+
+controller.open(...)
