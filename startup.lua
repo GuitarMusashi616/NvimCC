e5a66c7d7c045b2ad6c54720f35d793e006d3dbd
= <=>
2022-06-02 15:27:45 -0800

    added install file

---
+++ startup.lua
@@ -0,0 +1,14 @@
+function url(file, branch, repo)
+    branch = branch or "master"
+    repo = repo or "NvimCC"
+    return ("https://raw.githubusercontent.com/GuitarMusashi616/%s/%s/%s"):format(repo, branch, file)
+end
+
+local files = {"lib/class.lua", "model.lua", "view.lua", "controller.lua", "hjkl.lua"}
+
+for _, file in pairs(files) do
+    if fs.exists(file) then
+        shell.run("rm "..file)
+    end
+    shell.run("wget "..url(file))
+end
