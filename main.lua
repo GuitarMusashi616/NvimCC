27a378d109fc66b24cdcc1eaee27d946efee4f37
= <=>
2022-06-02 21:46:04 -0800

    fixed more changes

--- main.lua
+++ main.lua
@@ -9,9 +9,9 @@ local model = Model(controller)
 local view = View(controller)
 local hjkl = HJKL(controller)
 
-controller.set_model(model)
-controller.set_view(view)
+controller:set_model(model)
+controller:set_view(view)
 
-controller.add_event_listener(hjkl)
+controller:add_event_listener(hjkl)
 
-controller.open(...)
+controller:open(...)
--- model.lua
+++ model.lua
@@ -28,14 +28,23 @@ end
 
 function Model:scroll(n)
    self.y = math.min(math.max(1, self.y + n), #self.lines - self.NUM_LINES)
+   self.controller:notify({"model_scrolled", self.y, self.y + self.NUM_LINES})
 end
 
 function Model:update_cursor_horizontal(n)
     self.curX = math.max(1, math.min(self.curX + n, self.w))
+    self.controller:notify({"model_cursor_changed"})
 end
 
 function Model:update_cursor_vertical(n)
-    self.curY = math.max(1, math.min(self.curY + n, self.h))
+    if n > 0 and self.curY == self.h then
+        self:scroll(n)
+    elseif n < 0 and self.curY == 1 then
+        self:scroll(n)
+    else
+        self.curY = math.max(1, math.min(self.curY + n, self.h))
+        self.controller:notify({"model_cursor_changed"})
+    end
 end
 
 return Model
