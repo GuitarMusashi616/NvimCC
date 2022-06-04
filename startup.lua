e45a797b2d294e89aa4ef5a78801d02592acb629
= <=>
2022-06-03 18:13:08 -0800

    made it work better

--- startup.lua
+++ startup.lua
@@ -4,11 +4,27 @@ function url(file, branch, repo)
     return ("https://raw.githubusercontent.com/GuitarMusashi616/%s/%s/%s"):format(repo, branch, file)
 end
 
-local files = {"lib/class.lua", "model.lua", "view.lua", "controller.lua", "hjkl.lua", "nvim.lua"}
+local libs = {"class.lua", "tbl.lua", "util.lua"}
+local mains = {"model.lua", "view.lua", "controller.lua", "hjkl.lua", "nvim.lua"}
 
-for _, file in pairs(files) do
-    if fs.exists(file) then
-        shell.run("rm "..file)
+function dl(files)
+    for _, file in pairs(files) do
+        if fs.exists(file) then
+            shell.run("rm "..file)
+        end
+        shell.run("wget "..url(file))
     end
-    shell.run("wget "..url(file))
 end
+
+if fs.exists("lib") then
+    shell.run("rm lib")
+end
+
+shell.run("mkdir lib")
+shell.run("cd lib")
+
+dl(libs)
+
+shell.run("cd ..")
+
+dl(mains)
